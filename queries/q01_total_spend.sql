9f4e475e00febe7aa56762d20ed3d78f7784ca17
Sohan Punekar <61650293+sohan-19@users.noreply.github.com>
2026-05-08 12:09:16 +0530

    Delete queries directory

--- queries/q01_total_spend.sql
+++
@@ -1,4 +0,0 @@
--- Question 1: Find total spend per customer
-select c.customer_id,coalesce (sum(t.amount),0) as total_spend from customers c
-left join transactions as t on c.customer_id=t.customer_id
-group by c.customer_id;
