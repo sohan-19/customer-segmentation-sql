d4633738731cba75c9ced3e1628d510709d07f7b
Sohan Punekar <61650293+sohan-19@users.noreply.github.com>
2026-08-29 14:15:55 +0530

    Update q04_transaction_never_customer.sql

--- Basic Queries/q04_transaction_never_customer.sql
+++ Basic Queries/q04_transaction_never_customer.sql
@@ -1,6 +1,6 @@
 -- Question 4 Find customers who never made any transaction
-select c.customer_id from customers c
-left join transactions as t on c.customer_id=t.customer_id
-where t.transaction_id is null;
+SELECT c.customer_id FROM customers c
+LEFT JOIN transactions t ON c.customer_id = t.customer_id
+WHERE t.customer_id IS NULL;
 
 --It indicates all customers in the dataset have at least one transaction, so no unmatched records exist.
